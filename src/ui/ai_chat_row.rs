bde79ae242fb2f7d94274d5b2c0f31256ef057d5
Maciej Kopeć <maciejkopec92@gmail.com>
2025-06-12 18:38:40 +0200

    add copy button to chat messages

--- src/ui/ai_chat_row.rs
+++ src/ui/ai_chat_row.rs
@@ -1,6 +1,6 @@
 use adw::subclass::prelude::*;
 use gtk::{
-    glib::{self},
+    glib::{self, clone},
     prelude::*,
 };
 use std::cell::RefCell;
@@ -23,6 +23,8 @@ mod imp {
         pub message_label: TemplateChild<gtk::Label>,
         #[template_child]
         pub message_box: TemplateChild<gtk::Box>,
+        #[template_child]
+        pub copy_button: TemplateChild<gtk::Button>,
 
         pub message_type: RefCell<MessageType>,
     }
@@ -35,10 +37,52 @@ mod imp {
 
         fn class_init(klass: &mut Self::Class) {
             klass.bind_template();
+            klass.bind_template_callbacks();
         }
 
         fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
             obj.init_template();
+        }
+    }
+
+    #[gtk::template_callbacks]
+    impl ChatMessageRow {
+        #[template_callback]
+        fn on_copy_button_clicked(&self, _button: &gtk::Button) {
+            let text = self.message_label.text();
+            let clipboard = self.obj().clipboard();
+            clipboard.set_text(&text);
+
+            self.show_copy_feedback();
+        }
+    }
+
+    impl ChatMessageRow {
+        fn show_copy_feedback(&self) {
+            let copy_button = &self.copy_button;
+
+            if let Some(image) = copy_button
+                .child()
+                .and_then(|child| child.downcast::<gtk::Image>().ok())
+            {
+                image.set_icon_name(Some("object-select-symbolic"));
+            }
+            copy_button.add_css_class("copy-success");
+
+            glib::spawn_future_local(clone!(
+                #[weak]
+                copy_button,
+                async move {
+                    glib::timeout_future_seconds(1).await;
+                    if let Some(image) = copy_button
+                        .child()
+                        .and_then(|child| child.downcast::<gtk::Image>().ok())
+                    {
+                        image.set_icon_name(Some("edit-copy-symbolic"));
+                    }
+                    copy_button.remove_css_class("copy-success");
+                }
+            ));
         }
     }
 
